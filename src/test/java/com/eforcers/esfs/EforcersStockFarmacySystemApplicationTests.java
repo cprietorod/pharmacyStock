1889f72b570bb656726099ec48b0359c76b9f34c
carlos.prieto <carlos.prieto@eforcers.com>
2016-12-03 16:33:57 -0500

    fixed problem related with liquidbase and junit

--- src/test/java/com/eforcers/esfs/EforcersStockFarmacySystemApplicationTests.java
+++ src/test/java/com/eforcers/esfs/EforcersStockFarmacySystemApplicationTests.java
@@ -6,11 +6,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import static org.junit.Assert.assertEquals;
 
-//@RunWith(SpringRunner.class)
-//@SpringBootTest
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class EforcersStockFarmacySystemApplicationTests {
 
-	//@Test
+	@Test
 	public void contextLoads() {
 		assertEquals(6, 6);
 	}
