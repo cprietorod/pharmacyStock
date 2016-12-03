7b4995d7c3e50cc576b55d0cacc50db1551b9546
carlos.prieto <carlos.prieto@eforcers.com>
2016-12-03 16:55:21 -0500

    added Main Screen

---
+++ src/main/java/com/eforcers/esfs/gui/MainScreen.java
@@ -0,0 +1,28 @@
+package com.eforcers.esfs.gui;
+
+import java.awt.BorderLayout;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class MainScreen extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+
+
+
+	/**
+	 * Create the frame.
+	 */
+	public MainScreen() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+	}
+
+}
