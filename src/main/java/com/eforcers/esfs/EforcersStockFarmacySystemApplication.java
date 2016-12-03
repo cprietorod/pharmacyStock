68581a3e69c11d8cb40422084a33752bea57c07e
carlos.prieto <carlos.prieto@eforcers.com>
2016-12-03 16:18:41 -0500

    initial commit

---
+++ src/main/java/com/eforcers/esfs/EforcersStockFarmacySystemApplication.java
@@ -0,0 +1,12 @@
+package com.eforcers.esfs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EforcersStockFarmacySystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EforcersStockFarmacySystemApplication.class, args);
+	}
+}
---
+++ src/test/java/com/eforcers/esfs/EforcersStockFarmacySystemApplicationTests.java
@@ -0,0 +1,18 @@
+package com.eforcers.esfs;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import static org.junit.Assert.assertEquals;
+
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
+public class EforcersStockFarmacySystemApplicationTests {
+
+	//@Test
+	public void contextLoads() {
+		assertEquals(6, 6);
+	}
+
+}
