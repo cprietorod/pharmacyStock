b5f016a8c7dc4fdc46dd243b6b0ce600a3263edc
carlos.prieto <carlos.prieto@eforcers.com>
2016-12-03 16:56:04 -0500

    added main screen

--- src/main/java/com/eforcers/esfs/EforcersStockFarmacySystemApplication.java
+++ src/main/java/com/eforcers/esfs/EforcersStockFarmacySystemApplication.java
@@ -1,12 +1,35 @@
 package com.eforcers.esfs;
 
-import org.springframework.boot.SpringApplication;
+import java.awt.EventQueue;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+import com.eforcers.esfs.gui.MainScreen;
 
 @SpringBootApplication
 public class EforcersStockFarmacySystemApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EforcersStockFarmacySystemApplication.class, args);
+		//SpringApplication.run(EforcersStockFarmacySystemApplication.class, args);
+		new SpringApplicationBuilder(EforcersStockFarmacySystemApplication.class)
+        .headless(false)
+        .web(false)
+        .run(args);
 	}
+	
+	public EforcersStockFarmacySystemApplication(){
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					MainScreen frame = new MainScreen();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+	
+
+	
 }
